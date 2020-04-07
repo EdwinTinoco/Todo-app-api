1f558e86a9f52a8f92311208969e5a43278782f0
EdwinTinoco <jaredlotic@gmail.com>
2020-04-07 10:43:30 -0600

    Creating POST, PUT, DELETE endpoints

--- app.py
+++ app.py
@@ -39,10 +39,42 @@ def get_todos():
     result = todos_schema.dump(all_todos)
     return jsonify(result)
 
-
 # POST
-# PUT / PATCH
+@app.route('/todo', methods=["POST"])
+def add_todo():
+    title = request.json["title"]
+    done = request.json["done"]
+
+    new_todo = Todo(title, done)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    todo = Todo.query.get(new_todo.id)
+
+    return todo_schema.jsonify(todo)
+
+# PATCH
+@app.route("/todo/<id>", methods=["PATCH"])
+def update_todo(id):
+    todo = Todo.query.get(id)
+
+    new_done = request.json['done']
+    
+    todo.done = new_done
+
+    db.session.commit()
+    return todo_schema.jsonify(todo)
+
 # DELETE
+@app.route("/todo/<id>", methods=["DELETE"])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    
+    db.session.delete(todo)
+    db.session.commit()
+
+    return "Todo was successfully deleted"
 
 
 if __name__ == '__main__':
